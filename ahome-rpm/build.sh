68bfe3ed974f610dcae460cf98940a6d16fabe55
Danny <danny@afahounko.com>
2018-11-29 12:37:49 +0100

    Update build.sh

--- ahome-rpm/build.sh
+++ ahome-rpm/build.sh
@@ -2,18 +2,18 @@
 set -e
 mkdir -p /tmp/awx-rpmbuild-cache
 
-BUILD_SCRIPT=`pwd`/buildhelpers/build.sh
+BUILD_SCRIPT=`pwd`/ahome-rpm/buildhelpers/build.sh
 AHOME_TAR_GZ=awx-2.1.0.tar.gz
 AHOME_PATH=`pwd`/dist/$AHOME_TAR_GZ
 
 case "$1" in
     "amazonlinux-2017.03")
         DOCKER_IMAGE=ctbuild/amazonlinux:2017.03
-        YUM_CONFIG=`pwd`/buildhelpers/amazonlinux2017.03.yum.conf
+        YUM_CONFIG=`pwd`/ahome-rpm/buildhelpers/amazonlinux2017.03.yum.conf
     ;;
     "centos-7")
         DOCKER_IMAGE=ctbuild/centos:7
-        YUM_CONFIG=`pwd`/buildhelpers/centos7.yum.conf
+        YUM_CONFIG=`pwd`/ahome-rpm/buildhelpers/centos7.yum.conf
     ;;
     *)
         echo "Usage: $0 [centos-7|amazonlinux-2017.03]"
@@ -23,7 +23,7 @@ esac
 exec docker run --rm -i \
     -v `pwd`:/source \
     -v $AHOME_PATH:/source/$AHOME_TAR_GZ \
-    -v `pwd`/result:/result \
+    -v `pwd`/ahome-rpm/result:/result \
     -v /tmp/awx-rpmbuild-cache:/cache \
     -v $YUM_CONFIG:/etc/yum.conf \
     -v $BUILD_SCRIPT:/build.sh \
